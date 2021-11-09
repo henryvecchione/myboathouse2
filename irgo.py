8c5e291fbf11f5dd71f73a7e63b56470a523c545
henryvecchione <hjv@princeton.edu>
2021-11-09 01:12:21 -0500

    db responsive pages test

--- irgo.py
+++ irgo.py
@@ -3,6 +3,7 @@ from flask import render_template, Markup, flash, session, jsonify, abort
 import requests
 import os
 import urllib3
+import database as db
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -10,11 +11,26 @@ TEMPLATE_DIR = './templates'
 STATIC_DIR = './static'
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
-# app.secret_key = os.environ['secret_key']
-# app.config['SECRET_KEY'] = app.secret_key
 
 
 @app.route('/', methods=['GET'])
 def index():
     html = 'Hello world'
+    return make_response(html)
+
+
+@app.route('/allWorkouts', methods=['GET'])
+def allWorkouts():
+    wo = db.getAllWorkouts()
+    html = ''
+    for w in wo:
+        html += str(w) +"\n"
+    return make_response(html)
+
+@app.route('/allAthletes', methods=['GET'])
+def allAthletes():
+    ath = db.getAllAthletes()
+    html = ''
+    for a in ath:
+        html += str(a) + '\n'
     return make_response(html)
