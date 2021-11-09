591549636f290d466e522c7644f568833a1b5fef
henryvecchione <hjv@princeton.edu>
2021-11-09 02:04:46 -0500

    blank csv for download

--- csvRead.py
+++ csvRead.py
@@ -17,7 +17,7 @@ def csvReader(filename:str):
     csvFile.close()
 
     # first row of csv
-    date = lines[1][1].split('/')
+    date = lines[1][1].split('-')
     test = (lines[2][1] != str(0))
     pieces = lines[3][2:]
     notes = list(note for note in lines[4][2:] if note)
@@ -26,29 +26,51 @@ def csvReader(filename:str):
     for row in lines[5:]:
         athleteId = row[0]
         scores = []
-        scores = list(score for score in row[2:] if score) # terrible code. means add to list if not empy :)
+        scores = list(score for score in row[2:] if score) # terrible code. means add to list if not empy 
         scoresDict[athleteId] = scores
 
-    pprint(scoresDict)
 
     workoutDict = {
         'title' : autoTitle(pieces),
-        'date' : datetime.datetime(int(date[2])+2000, int(date[0]), int(date[1])),
-        'pieces' : list(p for p in pieces if p), # terrible code. means add to list if not empy :)
+        'date' : datetime.datetime(int(date[0]), int(date[1]), int(date[2])),
+        'pieces' : list(p for p in pieces if p), # terrible code. means add to list if not empy
         'scores' : scoresDict,
         'notes' : notes,
         'test' : test
     }
 
-    pprint(workoutDict)
+    return(workoutDict)
 
     
+def csvBlank(athleteList):
+    """ function for creating a blank CSV file of the format for the function above.
+        Takes a list of athletes with '_id' and 'name' fields in an iterable
+    """
+    filename = 'blank.csv'
+    blank = open(filename, 'w', newline='')
+    writer = csv.writer(blank)
+    today = str(datetime.date.today())
+    header = [\
+        ['MyBoathouse', '', '', '', '', '', '', ''], \
+        ['Date:', today, '', '', '', '', '', ''], \
+        ['Test:', '0', '', '', '', '', '', ''], \
+        ['Piece:', '', '(XXXXm / mm:ss)', '(XXXXm / mm:ss)', '(XXXXm / mm:ss)', '', '', ''], \
+        ['Notes:', '', '', '', '', '', '', '']\
+        ]
+    writer.writerows(header)
 
+    for a in athleteList:
+        row = [a['_id'], a['name'], '', '', '', '', '', '']
+        writer.writerow(row)
 
-
+    blank.close()
 
 
 
 
 if __name__ == "__main__":
-    csvReader('test.csv')
+    pprint(csvReader('test1.csv'))
+
+    athleteList = [{'_id' : 1, 'name' : 'Cal'}, {'_id' : 2, 'name' : 'Will'}, {'_id' : 3, 'name' : 'Ethan'}, {'_id' : 4, 'name' : 'Peter'}]
+
+    csvBlank(athleteList)
