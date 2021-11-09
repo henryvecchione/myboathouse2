3db7d2ebaea75b1ed7f7b1173064b3f7d497425c
henryvecchione <hjv@princeton.edu>
2021-11-08 23:01:00 -0500

    database methods

--- database.py
+++ database.py
@@ -1,2 +1,164 @@
+from pymongo import MongoClient
 import pymongo
+import secret
+import certifi
+
+CONNECTION_STRING = secret.database_url
+DBNAME = 'irgo'
+ATHLETE_COLLECTION = 'athletes'
+WORKOUT_COLLECTION = 'workouts'
+
+# ------------------------------------------------------------------- #
+# General Database methods
+
+def getDatabase():
+    client = pymongo.MongoClient(CONNECTION_STRING)
+    return client[DBNAME]
+
+def getCollection(collection):
+    try:
+        dbname = getDatabase()
+        return dbname[collection]
+    except Exception as e:
+        print(str(e))
+        return None
+# ------------------------------------------------------------------- #
+# general athlete collection methods 
+
+def addAthlete(athleteDict):
+    try:
+        collection_name = getCollection(ATHLETE_COLLECTION)
+        result = collection_name.insert_one(athleteDict)
+        return result.inserted_id
+    except Exception as e:
+        print(str(e))
+        return None
+
+def editAthlete(athleteId, field, newVal):
+    try:
+        collection_name = getCollection(ATHLETE_COLLECTION)
+        result = collection_name.update_one({'_id' : athleteId}, {'$set' : {field : newVal}})
+        return result.modified_count
+    except Exception as e:
+        print(str(e))
+        return None
+
+def queryAthlete(athleteId):
+    try:
+        collection_name = getCollection(ATHLETE_COLLECTION)
+        return collection_name.find_one({'_id' : athleteId})
+    except Exception as e:
+        print(str(e))
+        return None
+# ------------------------------------------------------------------- #
+# general workout collection methods 
+
+def addWorkout(workoutDict):
+    try:
+        collection_name = getCollection(WORKOUT_COLLECTION)
+        result = collection_name.insert_one(workoutDict)
+        return result.inserted_id
+    except Exception as e:
+        print(str(e))
+        return None
+
+def editWorkout(workoutId, field, newVal):
+    try:
+        collection_name = getCollection(WORKOUT_COLLECTION)
+        result = collection_name.update_one({'_id' : workoutId}, {'$set' : {field : newVal}})
+        return result.modified_count
+    except Exception as e:
+        print(str(e))
+        return None
+
+def queryWorkout(workoutId):
+    try:
+        collection_name = getCollection(WORKOUT_COLLECTION)
+        return collection_name.find_one({'_id' : workoutId})
+    except Exception as e:
+        print(str(e))
+        return None
+
+# ------------------------------------------------------------------- #
+
+# takes a workout ID, gets all participating athletes, and adds that 
+# workout to each athlete's 'workouts' array
+def attributeWorkout(workoutId):
+    try:
+        athletes_collection = getCollection(ATHLETE_COLLECTION)
+        workout_collection = getCollection(WORKOUT_COLLECTION)
+        workout = workout_collection.find_one({'_id' : workoutId})
+        participating = [int(k) for k in workout['scores'].keys()]
+        result = athletes_collection.update_many({'_id' : {'$in' : participating}}, {'$push' : {'workouts' : workoutId}})
+        return result.modified_count
+
+    except Exception as e:
+        print(str(e))
+        return None
+
+
+# ------------------------------------------------------------------- #
+# Test code
+
+if __name__ == "__main__":
+    athlete1 = {
+        "_id" : 69,
+        "name" : "Henry Vecchione",
+        "permissions" : ["admin"],
+        "prs" : {
+            "2000m" : "6:24",
+            "6000m" : "20:32"
+        },
+        "workouts" : [],
+        "side" : ["port"],
+        "class" : 2022,
+        "active" : True,
+        "awards" : {
+            "earc" : [],
+            "ira" : [],
+            "shirts" : ['g','de','n','da','t','p']
+        }
+    }
+    athlete2 = {
+        "_id" : 1,
+        "name" : "Cal Gorvy",
+        "permissions" : [],
+        "prs" : {
+            "2000m" : "5:59",
+            "6000m" : "17:24"
+        },
+        "workouts" : [],
+        "side" : ["starboard"],
+        "class" : 2025,
+        "active" : True,
+        "awards" : {
+            "earc" : ['4V'],
+            "ira" : ['1V'],
+            "shirts" : ['g','de','n','h','y','t','p']
+        }
+    }
+    workout1 = {
+        '_id' : 1,
+        'title' : '2x4000m, 3000m',
+        'date' : '11/08/21',
+        'pieces' : ['4000m', '4000m', '3000m'],
+        'scores' : {
+            '69' : ['15:12', '15:25' , '12:00'],
+            '1' : ['13:14', '14:20', '13:15']
+        },
+        'notes' : 'open rate',
+        'test' : False
+    }
+    print(queryAthlete(69))
+    print(queryAthlete(1))
+    print(editAthlete(69, 'prs', '6:24'))
+    print(editAthlete(1, 'side', ['port']))
+    print(queryAthlete(69))
+    print(queryAthlete(1))
+
+    print(queryWorkout(1))
+    print(attributeWorkout(1))
+    print(queryAthlete(69))
+    print(queryAthlete(1))
+
 
